12873b64da639c8ceb05c8b363f556aa90d5bad4
stojce <stojce@me.com>
2014-04-17 00:13:02 +0000

    mobile layout fix

--- views/layout/header.php
+++ views/layout/header.php
@@ -3,7 +3,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="copyright" content="Copyright (c) 2013 High Fidelity Inc. http://www.highfidelity.io" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php View::includeStyles(); ?>
     <?php View::includeScripts(); ?>
     <script type="text/javascript">
