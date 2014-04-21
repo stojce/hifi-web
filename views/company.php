c62f17c3ff5a126c4bee8a03d5e7744deeefe57d
Lucas Crisman <kordero@gmail.com>
2014-04-21 14:02:22 -0300

    some text css tweaks and investors card sizes/positioning changes

--- views/company.php
+++ views/company.php
@@ -5,42 +5,49 @@
         <ul>
             <li>
                 <h2>Presence</h2>
-                <p>
-                    We’re working with a wide variety of consumer-available motion 
-                    controllers, 3D cameras, and head-mounted displays. These amazing 
-                    new devices make real immersion possible, enabling virtual world 
-                    residents to animate avatar movements and facial expressions in real 
-                    time. Which we’ve observed leads to:
-                </p>
+                <p> We’re working with a wide variety of 
+                    consumer-available motion controllers, 3D 
+                    cameras, and head-mounted displays. 
+                    These amazing new devices make real 
+                    immersion possible, enabling virtual world 
+                    residents to animate avatar movements 
+                    and facial expressions in real time. Which 
+                    we’ve observed leads to: </p>
             </li>
             <li>
                 <h2>Connection</h2>
-                <p>
-                    Coupling these devices with high-speed networks and the vast computing 
-                    power many of us have on tap, High Fidelity is working to capture and 
-                    replicate the little details that underpin our interactions with others--things 
-                    like countenance, gaze, and spatialized audio cues. Which takes us to:
-                </p>
+                <p> Coupling these devices with 
+                    high-speed networks and the vast 
+                    computing power many of us have 
+                    on tap, High Fidelity is working to 
+                    capture and replicate the little details 
+                    that underpin our interactions with 
+                    others--things ike countenance, 
+                    gaze, and spatialized audio cues. 
+                    Which takes us to: </p>
             </li>
             <li>
                 <h2>Collaboration &amp; Commerce</h2>
+                <p> By borrowing computing time and bandwidth from 
+                    others, virtual events can grow to any size, space or 
+                    audience. High Fidelity is developing an exchange 
+                    which facilitates the lending and borrowing of 
+                    computing resources, using cryptocurrency credits 
+                    to recognize these transactions. These credits will 
+                    be used to buy digital goods and services from 
+                    others to help people build and power expansive 
+                    digital worlds. </p>
                 <p>
-                    By borrowing computing time and bandwidth from others, virtual events 
-                    can grow to any size, space or audience.  High Fidelity is developing 
-                    an exchange which facilitates the lending and borrowing of computing 
-                    resources, using cryptocurrency credits to recognize these transactions. 
-                    These credits will be used to buy digital goods and services from others 
-                    to help people build and power expansive digital worlds.
-                </p>
-                <p>
-                    <strong>The worlds created with High Fidelity can be linked together </strong>
-                    by the common avatar identity and digital marketplace we are building, operating 
-                    as a shared service to all users of the software platform. The open source software 
-                    is not yet complete, but is available on <a target="_blank" href="https://github.com">GitHub</a> 
-                    for experimentation and contribution. To supplement our in-house team, we have 
-                    also created a software development marketplace listing part-time paid opportunities – 
-                    you can find it at <a href="https://worklist.net">worklist.net</a>
-                </p>
+                    <strong>The worlds created with High Fidelity can be linked 
+                    together </strong> by the common avatar identity and digital 
+                    marketplace we are building, operating as a shared 
+                    service to all users of the software platform. The 
+                    open source software is not yet complete, but is 
+                    available on <a target="_blank" href="https://github.com">GitHub</a> for experimentation and 
+                    contribution. To supplement our in-house team, we 
+                    have also created a software development 
+                    marketplace listing part-time paid opportunities – you 
+                    can find it at <a href="https://worklist.net">worklist.net</a></p>
             </li>
         </ul>
     </article>
