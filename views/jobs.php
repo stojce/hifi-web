7c75f8dac422d201ec559206b7f8ca396c5e0acd
joanneci <joanneci@optonline.net>
2014-07-24 22:47:36 +0000

    new job tweaks

--- views/jobs.php
+++ views/jobs.php
@@ -1,7 +1,7 @@
 <?php
 $jobs = View::read('jobList'); 
 $tweetStrJob1 = urlencode('@highfidelityinc is working on building a new virtual world and they need a Graphics Engineer! Check out http://highfidelity.io/jobs #jobs #graphics');
-$tweetStrJob2 = urlencode('@highfidelityinc is working on building a new virtual world and they need a Technical Artist! Check out http://highfidelity.io/jobs #jobs #3dartists');
+$tweetStrJob2 = urlencode('@highfidelityinc is building a new virtual world and they need a Marketing Coordinator! Check out http://highfidelity.io/jobs #jobs #marketing');
 ?>
 <section id="particle-system"></section>
 <div role="main" id="main">
@@ -127,6 +127,13 @@ $tweetStrJob2 = urlencode('@highfidelityinc is working on building a new virtual
             <span>Tweet this job</span>
         </a>
     </article>
+    <article class="no-border-bottom get-our-attention">
+        <p><span class="strong">We are seeing lots of great applicants!</span> If you'd like to get our attention and also have
+        more fun than sitting in an interview, do the following:</p>
+        <p>Checkout our code on <a href="https://github.com/highfidelity/hifi">GitHub</a>, build the Interface client, and see if you
+        can login to Hifi. Then, take a look at <a href="https://worklist.net">Worklist</a> and see if there's a job you can do
+        to show us your work skills.</p>
+    </article>
     <article class="no-border-top radius-border-bottom">
         <p class="lead">Marketing Coordinator</p>
             <p>
@@ -149,17 +156,10 @@ $tweetStrJob2 = urlencode('@highfidelityinc is working on building a new virtual
                 <a href="mailto:hiring@highfidelity.io">hiring@highfidelity.io</a>
             </p>
         <a class="tweetthis" target="_blank"
-            href="http://twitter.com/home/?status=<?php echo $tweetStrJob1; ?>">
+            href="http://twitter.com/home/?status=<?php echo $tweetStrJob2; ?>">
             <i class="icon-twitter"></i>
             <span>Tweet this job</span>
         </a>
-    </article>
-    <article class="get-our-attention radius-border-bottom">
-        <p><span class="strong">We are seeing lots of great applicants!</span> If you'd like to get our attention and also have
-        more fun than sitting in an interview, do the following:</p>
-        <p>Checkout our code on <a href="https://github.com/highfidelity/hifi">GitHub</a>, build the Interface client, and see if you
-        can login to Hifi. Then, take a look at <a href="https://worklist.net">Worklist</a> and see if there's a job you can do
-        to show us your work skills.</p>
     </article>
     <h2><strong>2.</strong> Contract jobs</h2>
     <article class="radius-border-bottom radius-border-top">
