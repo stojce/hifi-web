277a8094c4c1e2393c9ebf56b8ca0aa764be4600
Lucas Crisman <kordero@gmail.com>
2014-08-13 19:05:19 -0300

    footer: some responsiveness support and markup cleaning

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -9,68 +9,66 @@ if (class_exists('View')) {
 
 ?>
     <footer id="site-links" role="contentinfo">
-        <div id="social">
-            <ul>
-                <li class="<?php echo $page == 'subscribe' ? 'selected' : ''; ?>">
-                    <a href="https://github.com/highfidelity" title="GitHub repository">
-                        <i class="icon-github icon-2x"></i>
-                        GitHub
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="https://worklist.net" title="Worklist">
-                        <img src="img/wl-logo.svg">
-                        Worklist
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="http://www.youtube.com/HighFidelityio" title="Contact us">
-                        <i class="icon-youtube icon-2x"></i>
-                        YouTube
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="http://twitter.com/highfidelityinc" title="Twitter">
-                        <i class="icon-twitter icon-2x"></i>
-                        Twitter
-                    </a>
-                </li>
-                <li>
-                    <a target="_blank" href="http://www.facebook.com/HighFidelityInc" title="Facebook">
-                        <i class="icon-facebook icon-2x"></i>
-                        Facebook
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:<?php echo CONTACT_EMAIL_ADDRESS; ?>" title="Email">
-                        <i class="icon-envelope-alt icon-2x"></i>
-                        Contact
-                    </a>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    <a href="#" title="Alpha">
-                        Alpha
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Docs">
-                        Docs
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Jobs">
-                        Jobs
-                    </a>
-                </li>
-                <li>
-                    <a href="#" title="Blog">
-                        Blog
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <ul>
+            <li class="<?php echo $page == 'subscribe' ? 'selected' : ''; ?>">
+                <a href="https://github.com/highfidelity" title="GitHub repository">
+                    <i class="icon-github icon-2x"></i>
+                    <span>GitHub</span>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="https://worklist.net" title="Worklist">
+                    <img src="img/wl-logo.svg">
+                    <span>Worklist</span>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.youtube.com/HighFidelityio" title="Contact us">
+                    <i class="icon-youtube icon-2x"></i>
+                    <span>YouTube</span>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="http://twitter.com/highfidelityinc" title="Twitter">
+                    <i class="icon-twitter icon-2x"></i>
+                    <span>Twitter</span>
+                </a>
+            </li>
+            <li>
+                <a target="_blank" href="http://www.facebook.com/HighFidelityInc" title="Facebook">
+                    <i class="icon-facebook icon-2x"></i>
+                    <span>Facebook</span>
+                </a>
+            </li>
+            <li>
+                <a href="mailto:<?php echo CONTACT_EMAIL_ADDRESS; ?>" title="Email">
+                    <i class="icon-envelope-alt icon-2x"></i>
+                    <span>Contact</span>
+                </a>
+            </li>
+        </ul>
+        <ul>
+            <li>
+                <a href="#" title="Alpha">
+                    Alpha
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Docs">
+                    Docs
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Jobs">
+                    Jobs
+                </a>
+            </li>
+            <li>
+                <a href="#" title="Blog">
+                    Blog
+                </a>
+            </li>
+        </ul>
         <p>
             <span>&copy; 2014 High Fidelity Inc</span>
         </p>
