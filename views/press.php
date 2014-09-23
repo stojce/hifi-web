3462c4a83719acf86725fd59d08d23663bc0e272
Lucas Crisman <kordero@gmail.com>
2014-09-23 18:32:52 -0300

    press: many tweaks + references

--- views/press.php
+++ views/press.php
@@ -6,20 +6,36 @@
     <section id="quotes">
         <ul>
             <li>
-                <blockquote>
-                    The expressive faces looked <em>incredibly human-like,</em>
-                    which adds a whole other dimension to the
-                    interaction by invoking <em>emotion</em>
-                </blockquote>
+                <blockquote>The expressive faces looked <em>incredibly
+                    human&#45;like,</em> which adds a whole other dimension
+                    to the interaction by invoking <em>emotion</em></blockquote>
                 <cite>quoVice News</cite>
             </li>
             <li>
-                <blockquote>
-                    With this incredibly innovative interface, <em>virtual realities,</em> 
-                    once exclusive to the fantastical world of gaming, <em>may very well 
-                    become an integral part of our daily lives</em>
-                </blockquote>
+                <blockquote>With this incredibly innovative interface,
+                    <em>virtual realities,</em>  once exclusive to the fantastical
+                    world of gaming, <em>may very well become an
+                    integral part of our daily lives</em></blockquote>
                 <cite>CNET</cite>
+            </li>
+        </ul>
+    </section>
+    <section id="references">
+        <ul>
+            <li>
+                <q cite="cnet">Philip Rosedale's Second Life with High Fidelity</q>
+            </li>
+            <li>
+                <q cite="new-scientist">Making Virtual Reality Less Virtual</q>
+            </li>
+            <li>
+                <q cite="mit-tech-review">Second Life 2.0: Virtual world recreates the real you</q>
+            </li>
+            <li>
+                <q cite="cnbc">Why Virtual Reality Will Compete with the Real World</q>
+            </li>
+            <li>
+                <q cite="the-atlantic">Avatars make online conferencing easy</q>
             </li>
         </ul>
     </section>
