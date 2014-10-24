10896d5820fe7e6605dac241259998189bc01c86
joanneci <joanneci@optonline.net>
2014-10-24 20:42:24 +0000

    add all press

--- views/press.php
+++ views/press.php
@@ -54,4 +54,154 @@
             </li>
         </ul>
     </section>
+<div class="container">
+    <div>
+        <a href="http://www.cnet.com/news/philip-rosedales-second-life-with-high-fidelity/" target="_blank">
+            <div>8/28/2014</div>
+            <div>Philip Rosedale to keynote OpenSim conference</div>
+            <div>Hyper Grid Business</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.seriouswonder.com/exponential-finance-living-virtual-world/" target="_blank">
+            <div>8/22/2014</div>
+            <div>Exponential Finance: Living In A Virtual World</div>
+            <div>Serious Wonder</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://voicesofvr.com/25-philip-rosedale-on-high-fidelity-virtual-body-language-telepresence-3d-audio-distributed-computing-voxels-cryptocurrencies-the-future-of-the-metaverse/" target="_blank">
+            <div>6/16/2014</div>
+            <div>Philip Rosedale on High Fidelity, virtual body language, telepresence, 3D audio, 
+                 distributed computing, Voxels, cryptocurrencies & the future of the Metaverse</div>
+            <div>Voices of VR Podcast</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.hypergridbusiness.com/2014/06/how-high-fidelity-will-make-money-while-giving-away-software/" target="_blank">
+            <div>6/13/2014</div>
+            <div>How High Fidelity will make money while giving away software</div>
+            <div>Hyper Grid Business</div>
+       </a>
+    </div>
+    <div>
+        <a href="http://www.cnbc.com/id/101747980" target="_blank">
+            <div>6/11/2014</div>
+            <div>Avatars make online conferencing easy</div>
+            <div>CNBC</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.polygon.com/2014/5/20/5735262/high-fidelity-second-life" target="_blank">
+            <div>5/20/2014</div>
+            <div>Second Life creator's next challenge: pairing rich interaction with VR</div>
+            <div>Polygon</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.creativeshed.com/2014/05/philip-rosedale-addressed-silicon-valley-vr/" target="_blank">
+            <div>5/20/2014</div>
+            <div>Philip Rosedale Addresses Silicon Valley VR, Demos High Fidelity</div>
+            <div>Creative Shed</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.theverge.com/2014/5/19/5732386/high-fidelity-philip-rosendale-telepresence-second-life-hands-on" target="_blank">
+            <div>5/19/2014</div>
+            <div>Inside High Fidelity, the virtual reality successor to 'Second Life'</div>
+            <div>The Verge</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.roadtovr.com/svvr-expo-2014-keynote-talk-philip-rosedale-ceo-high-fidelity-livestream-2pm-pdt/" target="_blank">
+            <div>5/19/2014</div>
+            <div>SVVR Con: Keynote Talk by Philip Rosedale, CEO High Fidelity &#45; LiveStream @ 2pm PDT</div>
+            <div>RoadtoVR</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.technologyreview.com/view/527101/why-virtual-reality-will-compete-with-the-real-world/" target="_blank">
+            <div>5/5/2014</div>
+            <div>Why Virtual Reality Will Compete with the Real World</div>
+            <div>MIT Technology Review</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://motherboard.vice.com/en_uk/read/creepily-realistic-avatars-are-giving-second-life-a-second-life" target="_blank">
+            <div>4/25/2014</div>
+            <div>Creepily Realistic Avatars Are Giving 'Second Life' a Second Life</div>
+            <div>Vice News</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.newscientist.com/article/mg22229664.000-second-life-20-virtual-world-recreates-the-real-you.html#.VBCqH0iKyW4" target="_blank">
+            <div>4/24/2014</div>
+            <div>Second Life 2.0: Virtual world recreates the real you</div>
+            <div>New Scientist</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.theatlantic.com/technology/archive/2013/12/making-virtual-reality-less-virtual/282640/" target="_blank">
+            <div>12/27/2013</div>
+            <div>Making Virtual Reality Less Virtual</div>
+            <div>The Atlantic</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.awntv.com/videos/professional-spotlight-philip-rosedale-siggraph-asia-2013-part-1" target="_blank">
+            <div>11/20/2013</div>
+            <div>Professional Spotlight: Philip Rosedale at SIGGRAPH ASIA 2013 Part 1</div>
+            <div>Animated World Network Television</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.infowars.com/google-funds-creation-of-secretive-avatar-style-virtual-reality/" target="_blank">
+            <div>11/13/2013</div>
+            <div>Google funds creation of secretive avatar-style Virtual Reality</div>
+            <div>InfoWars</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://singularityhub.com/2013/11/12/second-life-founder-philip-rosedale-is-quietly-creating-a-next-generation-virtual-world/" target="_blank">
+            <div>11/12/2013</div>
+            <div>Second Life Founder, Philip Rosedale, is Quietly Creating a Next-Generation Virtual World</div>
+            <div>Singularity Hub</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://twit.tv/show/triangulation/116" target="_blank">
+            <div>8/21/2013</div>
+            <div>Triangulation 116: Philip Rosedale</div>
+            <div>This Week in Tech</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.cnet.com/news/philip-rosedales-second-life-with-high-fidelity/" target="_blank">
+            <div>6/4/2013</div>
+            <div>Philip Rosedale's Second Life with High Fidelity</div>
+            <div>CNET</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://www.technologyreview.com/view/514776/second-life-founders-new-virtual-world-uses-body-tracking-hardware/" target="_blank">
+            <div>5/20/2013</div>
+            <div>Second Life Founder's New Virtual World Uses Body-Tracking Hardware</div>
+            <div>MIT Technology Review</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://techcrunch.com/2013/04/19/true-ventures-backs-high-fidelity/" target="_blank">
+            <div>4/19/2013</div>
+            <div>True Ventures Confirms Investment In Second Life Founder Philip Rosedale's New Startup High Fidelity</div>
+            <div>TechCrunch</div>
+        </a>
+    </div>
+    <div>
+        <a href="http://pulse2.com/2013/04/05/second-life-founder-philip-rosedale-raises-2-4-million-for-new-startup-high-fidelity-83525/" target="_blank">
+            <div>4/5/2013</div>
+            <div>Second Life Founder Philip Rosedale Raises $2.4 Million For New Startup, High Fidelity</div>
+            <div>Pulse 2.0</div>
+        </a>
+    </div>
+</div>
 </div>
