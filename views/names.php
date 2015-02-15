989b5429bc8a1e679a4ef0d993814cfec1ef174a
Lucas Crisman <kordero@gmail.com>
2015-02-15 19:00:42 -0300

    some small changes at names page per private chat request

--- views/names.php
+++ views/names.php
@@ -1,8 +1,5 @@
 <div id="container">
   <div role='main' id='main'>
-    <h1>
-      Place Names are the .com's &amp; .net's of the Metaverse
-    </h1>
     <p>
       Where domain names get you the web pages, Place Names
       get you to a fixed location in 3D space.
@@ -12,7 +9,6 @@
       Welcome Area somewhere in Tokyo. The exact location for
       the Place Name is determined by its owner.
     </p>
-    <p>They cost $20/year</p>
     <p>
       <a href=''>Buy a Place Name</a>
     </p>
