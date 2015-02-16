d3a2b8ffb9ab9957220850c197686a823a375940
Lucas Crisman <kordero@gmail.com>
2015-02-16 18:08:00 -0300

    resolving conflicts at views/home.php

--- views/home.php
+++ views/home.php
@@ -6,52 +6,32 @@
       <a href="hifi://Free">
         <img src="img/home/places/small/Free.jpg" />
         <h4>Free</h4>
-<<<<<<< HEAD
         <p>Operated by Judas</p>
         <p>Old-time factory.</p>
-=======
-        <p>Old-time factory.</p>
-        <p>operated by High Fidelity</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://Heron">
         <img src="img/home/places/small/Heron.jpg" />
         <h4>Heron</h4>
-<<<<<<< HEAD
         <p>Operated by OmegaHeron</p>
         <p>Country Home.</p>
-=======
-        <p>Country Home.</p>
-        <p>operated by High Fidelity</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://Ireland">
         <img src="img/home/places/small/Ireland.png" />
         <h4>Ireland</h4>
-<<<<<<< HEAD
         <p>Operated by Derric Foggarty</p>
         <p>Space shuttle pub.</p>
-=======
-        <p>Space shuttle pub.</p>
-        <p>operated by Judas</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://Openvue">
         <img src="img/home/places/small/Openvue.jpg" />
         <h4>Openvue</h4>
-<<<<<<< HEAD
         <p>Operated by Ai_Austin</p>
         <p>Open Virtual University of Edinburgh.</p>
-=======
-        <p>Open Virtual University of Edinburgh.</p>
-        <p>operated by OmegaHeron</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
@@ -63,52 +43,32 @@
       <a href="hifi://porto">
         <img src="img/home/places/small/porto.jpg" />
         <h4>Porto</h4>
-<<<<<<< HEAD
         <p>Operated by High Fidelity</p>
         <p>Mountain hideaway.</p>
-=======
-        <p>Mountain hideaway.</p>
-        <p>operated by OmegaHeron</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://rivenglen">
         <img src="img/home/places/small/rivenglen.jpg" />
         <h4>Rivenglen</h4>
-<<<<<<< HEAD
         <p>Operated by High Fidelity</p>
         <p>Mead for my men!</p>
-=======
-        <p>Mead for my men!</p>
-        <p>operated by OmegaHeron</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://Venice">
         <img src="img/home/places/small/Venice.png" />
         <h4>Venice</h4>
-<<<<<<< HEAD
         <p>Operated by Chris</p>
         <p>Italian gem.</p>
-=======
-        <p>Italian gem.</p>
-        <p>operated by OmegaHeron</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
     <li>
       <a href="hifi://Adriania">
         <img src="img/home/places/small/adriania.jpg" />
         <h4>Adriania</h4>
-<<<<<<< HEAD
         <p>Operated by Adrian</p>
         <p>Town center of Adriania.</p>
-=======
-        <p>Town center of Adriania.</p>
-        <p>operated by OmegaHeron</p>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       </a>
     </li>
   </ul>
@@ -164,20 +124,13 @@
       </fieldset>
       <div id="downloadfade">
         <div>
-<<<<<<< HEAD
           <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-=======
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
           <p>
             Coming very soon! Give us your email and we'll get in touch
             when Interface, Stack Manager and Place Names are available.
           </p>
           <input type="email" name="EMAIL" placeholder="your@email.com" required />
-<<<<<<< HEAD
           <button type="submit" name="subscribe">Sign up</button>
-=======
-          <button type="submit" name="subscribe">Sign Up</button>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
         </div>
       </div>
     </form>
@@ -186,11 +139,7 @@
 <div id="container">
   <section id="company">
     <article>
-<<<<<<< HEAD
       <h2>The company</h2>
-=======
-      <h3>The company</h3>
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
       <p class="lead">
         We believe that both the hardware and the internet infrastructure <strong>are now available
         to give people around the world access to an interconnected Metaverse</strong> that will offer
@@ -248,13 +197,8 @@
             <h3>Ryan Karpf</h3>
             <p>
               Design-minded. Thinks MVP has too 
-<<<<<<< HEAD
               many letters.
               Loves Syd Mead and making fresh pasta.
-=======
-              many letters. Loves Syd Mead and 
-              making fresh pasta.
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
             </p>
           </figure>
         </li>
@@ -274,12 +218,8 @@
             <img src="img/team/grayson.gif" class="img-circle" alt="Grayson Stebbins" />
             <h3>Grayson Stebbins</h3>
             <p>
-<<<<<<< HEAD
               Designer &amp; generalist. <br />
               Amateur carpenter. <br />
-=======
-              Designer &amp; generalist. Amateur carpenter. 
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
               Analog, digital, universal.
             </p>
           </figure>
@@ -301,13 +241,8 @@
             <h3>Stephen Birarda</h3>
             <p>
               Constant refactorer. Believes that 
-<<<<<<< HEAD
               programming is art. <br />
               Obsessive hockey fan, secret ping pong champion.
-=======
-              programming is art. Obsessive hockey 
-              fan, secret ping pong champion.
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
             </p>
           </figure>
         </li>
@@ -329,11 +264,7 @@
             <p>
               Manages office systems to make life easier. 
               Fast talker, avid learner, novice 
-<<<<<<< HEAD
               salsa dancer, and great cook. 
-=======
-              salsa dancer, <br/>and great cook. 
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
             </p>
           </figure>
         </li>
@@ -398,7 +329,6 @@
     <article>
       <h2>Investors</h2>
       <ul>
-<<<<<<< HEAD
         <li>
           <h3>True Ventures</h3>
         </li>
@@ -415,42 +345,6 @@
           <h3>Linden Lab</h3>
         </li>
         <li>
-=======
-        <li
-          data-0="left: 2000px; transform:rotate(-90deg);"
-          data-4320="left: 2000px; transform:rotate(-90deg);"
-          data-4720="left: -80px; transform:rotate(-4deg);">
-          <h3>True Ventures</h3>
-        </li>
-        <li
-          data-0="left: 2000px; transform:rotate(90deg); margin-top: 5px;"
-          data-4360="left: 2000px; transform:rotate(90deg); margin-top: 5px;"
-          data-4760="left: 135px; transform:rotate(5deg);">
-          <h3>Vulcan Capital</h3>
-        </li>
-        <li
-          data-0="left: 2000px; transform:rotate(160deg); margin-top: 11px;"
-          data-4300="left: 2000px; transform:rotate(160deg); margin-top: 11px;"
-          data-4700="left: 355px; transform:rotate(2deg);">
-          <h3>Kapor Capital</h3>
-        </li>
-        <li
-          data-0="left: 2000px; transform:rotate(-160deg); margin-top: 185px;"
-          data-4370="left: 2000px; transform:rotate(-160deg); margin-top: 185px;"
-          data-4770="left: -93px; transform:rotate(0deg);">
-          <h3>Google Ventures</h3>
-        </li>
-        <li
-          data-0="left: 2000px; transform:rotate(-160deg); margin-top: 185px;"
-          data-4440="left: 2000px; transform:rotate(-160deg); margin-top: 185px;"
-          data-4840="left: 126px; transform:rotate(4deg);">
-          <h3>Linden Lab</h3>
-        </li>
-        <li
-          data-0="left: 2000px; transform:rotate(-160deg); margin-top: 200px;"
-          data-4480="left: 2000px; transform:rotate(-160deg); margin-top: 200px;"
-          data-4880="left: 340px; transform:rotate(0deg);">
->>>>>>> 0cc55ab462e4843e4e35b6b6e7f97f895dbe6896
           <p>And an intriguing assortment of forward-looking angels.</p>
         </li>
       </ul>
