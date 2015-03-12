6a58391c468e54d033f1fbcbf6647da93271fcde
Lucas Crisman <kordero@gmail.com>
2015-03-12 03:18:03 -0300

    refactoring download/ js & OS suggestion fixes

--- download/js/download.js
+++ download/js/download.js
@@ -1,84 +1,82 @@
-$(function() {
-     // proxima-nova fonts
-    if (typeof Typekit != 'undefined') {
-        Typekit.load();
-    }
-    var browser_version =  {    "mac"  : "Requires Mountain Lion(10.8) or newer",
-                                "windows" : "Requires Windows 7.1 or newer",
-                                "linux" : "Requires  ubuntu 13.04 or newer"
-                           };
-    var projectsData = [];
-    if ($.cookie('interface-os')) {
-        $('.interface .choose-os option').removeAttr('selected');
-        $('.interface .choose-os option[value="'+ $.cookie('interface-os') +'"]').attr('selected', 'selected');
-        $('.interface .os-icon').removeClass('selected');
-        $('.interface .os-icon-' + $.cookie('interface-os')).addClass('selected');
-        $('.interface .choose-os-container p').text(browser_version[$.cookie('interface-os')]);
-    }
+var download = {
+    projectsData: [],
+    os_message: {
+        "mac"  : "Requires Mountain Lion(10.8) or newer",
+        "windows" : "Requires Windows 7.1 or newer",
+        "linux" : "Requires  ubuntu 13.04 or newer"
+    },
 
-    if ($.cookie('stackmanager-os')) {
-        $('.stackmanager .choose-os option').removeAttr('selected');
-        $('.stackmanager .choose-os option[value="'+ $.cookie('stackmanager-os') +'"]').attr('selected', 'selected');
-        $('.stackmanager .os-icon').removeClass('selected');
-        $('.stackmanager .os-icon-' + $.cookie('stackmanager-os')).addClass('selected');
-        $('.stackmanager .choose-os-container p').text(browser_version[$.cookie('stackmanager-os')]);
-    }
+    init: function() {
+         // proxima-nova fonts
+        if (typeof Typekit != 'undefined') {
+            Typekit.load();
+        }
+        download.getBuilds(function() {
+            download.initDropdowns();
+            download.suggestPackage();
+        });
 
-    $('.interface .choose-os').chosen().change(function() {
-        $.cookie('interface-os', $(this).val());
-        $('.interface .os-icon').removeClass('selected');
-        $('.interface .os-icon-' + $.cookie('interface-os')).addClass('selected');
-        $('.interface .choose-os-container p').text(browser_version[$.cookie('interface-os')]);
-        renderBuild('interface', $(this).val());
-    });
+    },
 
-    $('.stackmanager .choose-os').chosen().change(function() {
-        $.cookie('stackmanager-os', $(this).val());
-        $('.stackmanager .os-icon').removeClass('selected');
-        $('.stackmanager .os-icon-' + $.cookie('stackmanager-os')).addClass('selected');
-        $('.stackmanager .choose-os-container p').text(browser_version[$.cookie('stackmanager-os')]);
-        renderBuild('stackmanager', $(this).val());
-    });
+    initDropdowns: function() {
+        $('.interface .choose-os').chosen().change(function() {
+            $('.interface .os-icon').removeClass('selected');
+            $('.interface .os-icon-' + $(this).val()).addClass('selected');
+            $('.interface .choose-os-container p').text(download.os_message[$(this).val()]);
+            download.renderBuild('interface', $(this).val());
+            $(this).trigger('chosen:updated');
+        });
 
-    $.get('/builds.xml', function(data) {
-        var projects = $($(data).find('project'));
-        for (projectIndex = 0; projectIndex < projects.length; projectIndex++) {
-            var project = $(projects[projectIndex]);
-            projectsData[$(project).attr('name')] = [];
-            var platforms = $(project).find('platform');
-            for (platformIndex = 0; platformIndex < platforms.length; platformIndex++) {
-                var platform = $(platforms[platformIndex]);
-                projectsData[$(project).attr('name')][$(platform).attr('name')] = [];
-                var buildVersions = $(platform).find("build");
-                for (buildIndex = 0; buildIndex < buildVersions.length; buildIndex++) {
-                    var notes = $(buildVersions[buildIndex]).find('note');
-                    var notesHtml = null;
-                    if (typeof (notes) != "undefined" && notes.length > 0) {
-                        notesHtml  = "<ul>";
-                        for (nodeIndex = 0; nodeIndex < notes.length; nodeIndex++) {
-                            notesHtml += "<li><p>" + $(notes[nodeIndex]).text() + "</p></li>" ;
+        $('.stackmanager .choose-os').chosen().change(function() {
+            $('.stackmanager .os-icon').removeClass('selected');
+            $('.stackmanager .os-icon-' + $(this).val()).addClass('selected');
+            $('.stackmanager .choose-os-container p').text(download.os_message[$(this).val()]);
+            download.renderBuild('stackmanager', $(this).val());
+            $(this).trigger('chosen:updated');
+        });
+    },
+
+    getBuilds: function(fAfter) {
+        $.get('../builds.xml', function(data) {
+            var projects = $($(data).find('project'));
+            for (projectIndex = 0; projectIndex < projects.length; projectIndex++) {
+                var project = $(projects[projectIndex]);
+                download.projectsData[$(project).attr('name')] = [];
+                var platforms = $(project).find('platform');
+                for (platformIndex = 0; platformIndex < platforms.length; platformIndex++) {
+                    var platform = $(platforms[platformIndex]);
+                    download.projectsData[$(project).attr('name')][$(platform).attr('name')] = [];
+                    var buildVersions = $(platform).find("build");
+                    for (buildIndex = 0; buildIndex < buildVersions.length; buildIndex++) {
+                        var notes = $(buildVersions[buildIndex]).find('note');
+                        var notesHtml = null;
+                        if (typeof (notes) != "undefined" && notes.length > 0) {
+                            notesHtml  = "<ul>";
+                            for (nodeIndex = 0; nodeIndex < notes.length; nodeIndex++) {
+                                notesHtml += "<li><p>" + $(notes[nodeIndex]).text() + "</p></li>" ;
+                            }
+                            notesHtml += "</ul>";
                         }
-                        notesHtml += "</ul>";
+
+                        var build = {
+                                    "version" : $(buildVersions[buildIndex]).find('version').text(),
+                                    "url" : $(buildVersions[buildIndex]).find('url').text(),
+                                    "time" : $(buildVersions[buildIndex]).find('timestamp').text(),
+                                    "notes" : notesHtml
+                        };
+                       download.projectsData[$(project).attr('name')][$(platform).attr('name')][buildIndex] = build;
                     }
 
-                    var build = {
-                                "version" : $(buildVersions[buildIndex]).find('version').text(),
-                                "url" : $(buildVersions[buildIndex]).find('url').text(),
-                                "time" : $(buildVersions[buildIndex]).find('timestamp').text(),
-                                "notes" : notesHtml
-                    };
-                   projectsData[$(project).attr('name')][$(platform).attr('name')][buildIndex] = build;
                 }
-
             }
-        }
+            if (fAfter) {
+                fAfter();
+            }
+        });
+    },
 
-        renderBuild('interface',    $.cookie('interface-os') ? $.cookie('interface-os')  : "mac");
-        renderBuild('stackmanager', $.cookie('stackmanager-os') ? $.cookie('stackmanager-os') : "windows" );
-    });
-
-    function renderBuild(project, platform) {
-        var builds =  projectsData[project][platform];
+    renderBuild: function(project, platform) {
+        var builds =  download.projectsData[project][platform];
         $('#' + platform).find('.choose-build-version-container div.chosen-container').remove();
         var html = "<select class='choose-build-version'>";
         for (var index  in builds) {
@@ -101,6 +99,31 @@ $(function() {
                 $('#' + project).find('.release-notes').html("<b>Release notes</b>" + builds[$(this).val()]['notes']);
             }
         });
-    }
+    },
 
+    suggestPackage: function() {
+        $('.choose-os').val('');
+        var ua = navigator.userAgent;
+        var appVersion = navigator.appVersion;
+        if (ua.match("Win")) {
+            $('.choose-os').val('windows');
+            return;
+        }
+        if (ua.match("Mac")) {
+            $('.choose-os').val('mac');
+            return;
+        }
+        /*
+        if (ua.match("Linux") || appVersion.match("X11")) {
+            $('.choose-os').val('linux').change();
+            return;
+        }
+        */
+        $('.choose-os').change();
+    },
+
+};
+
+$(function() {
+    download.init();
 });
